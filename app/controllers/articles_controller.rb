85ca41696a6bb7b1d47bb7ce251eb626d5d60c89
XinHui <xhleong@Leongs-MacBook-Pro.local>
2018-02-14 15:39:40 +0800

    Create show functions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,11 +3,26 @@ class ArticlesController < ApplicationController
 	def new
 	end
 
-	def create
-		
-		render plain: params[:article].inspect
+	def show
+
+		@article = Article.find(params[:id])
 
 	end
+
+	def create
+		# render plain: params[:article].inspect
+		@article = Article.create(article_params)
+		redirect_to @article
+
+	end
+
+
+
+	private
+
+		def article_params
+			params.require(:article).permit(:title, :text)
+		end
 
 
 end
