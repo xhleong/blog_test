7e80eda18b4910babbf2c6a80826f585470454e0
XinHui <xhleong@Leongs-MacBook-Pro.local>
2018-02-21 14:33:44 +0800

    Added customer helper method to highlight active links in nav bar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  #if on active page, color = white. Else, color = default
+  #take arguments: link, current page, path_name(url of the link) change font color
+  def active_link_to (nav_link, contrl, action, url,style)
+    if controller_name == contrl && action_name == action
+      return link_to nav_link, url, id:style
+    else
+      return link_to nav_link, url, class:style
+    end
+  end
+
 end
